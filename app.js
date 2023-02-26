5511071748a6090f2568788a13070d465e750cf5
bparvizm79 <bparvizm79gmail.com>
2023-02-26 14:06:29 +0500

    add desinet

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
+let wavesurfer = WaveSurfer.create({
+    container: '#waveform'
+});
+
 $(".search_btn").click(function(){
     const word = $(".search").val()
     axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then((res)=>{
         const data = res.data[0]
         console.log(data)
-        const sound = new Audio(data.phonetics[0].audio)
-        $(".dictionary").html(`
-        <h1>${data.word}</h1>
-        <button class="sound">click to sound</button>
-        <p>${data.meanings[0].definitions[0].definition}</p>
-        <h2>sinonym</h2>
-        <ul>
-            ${data.meanings[0].synonyms.map((val)=>(
-                `<li>${val}</li>`
-            ))}
-        </ul>
-        <h2>antonym</h2>
-        <ul>
-        ${data.meanings[0].antonyms.map((val)=>(
-            `<li>${val}</li>`
-        ))}
-        </ul>
-        `)
-        $(".sound").click(function(){
-            sound.play()
+        // const sound = new Audio(data.phonetics[0].audio)
+      $(".title").text(data.word) 
+ wavesurfer.load(data.phonetics[0].audio);
+
+      $(".discription p").text(data.meanings[0].definitions[0].definition) 
+      $(".sinonym").html(data.meanings[0].synonyms.map((val)=>(
+        `<li>${val}</li>`
+    )))
+    $(".antonym").html(data.meanings[0].antonyms.map((val)=>(
+        `<li>${val}</li>`
+    )))
+      $(".sound_btn").click(function(){
+            // sound.play()
+            wavesurfer.play()
         })
     })
 })
